b9b9899a21a1f22fa8e0fb6b4db5f57c89f5eddf
s24577 <s24577@pjwstk.edu.pl>
2024-06-26 16:45:16 +0200

    add GET & POST

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Post} from "./post.model";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    console.log(postData);
+    this.http
+      .post<{ name: string }>('https://first-project-c5fcf-default-rtdb.europe-west1.firebasedatabase.app/posts.json', postData)
+       .subscribe(responseData =>{console.log(responseData)});
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
-  onClearPosts() {
-    // Send Http request
+  onClearPosts(postData: Post) {
+  }
+
+  private fetchPosts(){
+    this.http.get<{[key: string]: Post}>('https://first-project-c5fcf-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+      .pipe(
+        map(responseData  => {
+        const postsArray: Post[] = [];
+        for (const key in responseData){
+          if (responseData.hasOwnProperty(key)){
+            postsArray.push({...responseData[key], id: key});
+          }
+        }
+        return postsArray;
+      })
+      ).subscribe(posts => {
+        console.log(posts);
+      });
   }
 }
